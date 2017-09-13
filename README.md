8817e58c78065f829777f33100d459cfefeced2a
LitoMore <litomore@gmail.com>
2017-09-14 00:27:42 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# fanfou-desktop-notifier
+
+Fanfou Desktop Notifier
+
+## Usage
+
+1. Clone this repository
+
+```bash
+$ git clone https://github.com/LitoMore/fanfou-desktop-notifier.git
+```
+
+2. Install Dependencies
+
+```bash
+$ cd fanfou-desktop-notifier
+$ npm install
+```
+
+3. Create config file
+
+```bash
+$ cp config.json.example config.json
+```
+
+4. Config your account in `config.json`
+
+```json
+{
+  "CONSUMER_KEY": "consumer_key",
+  "CONSUMER_SECRET": "consumer_secret",
+  "OAUTH_TOKEN": "oauth_token",
+  "OAUTH_TOKEN_SECRET": "oauth_token_secret"
+}
+```
+
+5. Launch notifier
+
+```bash
+$ node app.js
+```
+
+Or launch with [pm2](https://github.com/Unitech/PM2)
+
+```bash
+$ pm2 start app.js
+```
+
+## Related
+
+- [fanfou-sdk](https://github.com/LitoMore/fanfou-sdk-node) - Fanfou SDK for Node.js
+- [fanfou-streamer](https://github.com/LitoMore/fanfou-streamer) - Fanfou Streaming API SDK for Node.js
+
+## License
+
+MIT @ [LitoMore](https://github.com/LitoMore)
---
+++ app.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const Streamer = require('fanfou-streamer')
+const notifier = require('node-notifier')
+const Fanfou = require('fanfou-sdk')
+
+const {
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+  OAUTH_TOKEN,
+  OAUTH_TOKEN_SECRET
+} = process.env
+
+const streamer = new Streamer({
+  consumerKey: CONSUMER_KEY,
+  consumerSecret: CONSUMER_SECRET,
+  oauthToken: OAUTH_TOKEN,
+  oauthTokenSecret: OAUTH_TOKEN_SECRET
+})
+
+const ff = new Fanfou({
+  auth_type: 'oauth',
+  consumer_key: CONSUMER_KEY,
+  consumer_secret: CONSUMER_SECRET,
+  oauth_token: OAUTH_TOKEN,
+  oauth_token_secret: OAUTH_TOKEN_SECRET
+})
+
+const replyBox = (message, res) => {
+  notifier.notify({
+    title: '饭否',
+    message,
+    reply: true
+  }, (error, response, metadata) => {
+    if (!error && metadata && metadata.activationType === 'replied') {
+      ff.post('/statuses/update', {
+        in_reply_to_status_id: res.object.id,
+        status: `@${res.source.screen_name} ${metadata.activationValue}`
+      }, () => {})
+    }
+  })
+}
+
+// Mentions
+streamer.on('message.mention', res => {
+  replyBox(`@${res.source.screen_name} 提到了你\n${res.object.text}`, res)
+})
+
+// Reply
+streamer.on('message.reply', res => {
+  replyBox(`@${res.source.screen_name} 回复了你\n${res.object.text}`, res)
+})
+
+// Repost
+streamer.on('message.repost', res => {
+  replyBox(`@${res.source.screen_name} 转发了\n${res.object.text}`, res)
+})
+
+// Add fav
+streamer.on('fav.create', res => {
+  replyBox(`@${res.source.screen_name} 收藏了\n${res.object.text}`, res)
+})
+
+// Del fav
+streamer.on('fav.delete', res => {
+  replyBox(`@${res.source.screen_name} 取消收藏了\n${res.object.text}`, res)
+})
+
+streamer.start()
